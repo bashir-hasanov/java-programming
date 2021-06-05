25b87b022c81e11ae38d19547d80d2cc1d053528
Bashir Hasanov <beshir.hasanov@gmail.com>
2021-06-05 11:38:53 -0400

    Added new package day50 and 2 classes

---
+++ src/day50_inheritance/StaticBlockDemo.java
@@ -0,0 +1,21 @@
+package day50_inheritance;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StaticBlockDemo {
+    static int num;
+    static List<String> carModels;
+    static {
+        System.out.println("static initializer block");
+        num = 10;
+        carModels = new ArrayList<>();
+        carModels.addAll(Arrays.asList("Ford", "Honda", "Toyota", "BMW", "Mercedes-Benz"));
+    }
+
+    public StaticBlockDemo() {
+        System.out.println("No-ARGS constructor");
+        num += 5;
+    }
+}
---
+++ src/day50_inheritance/StaticBlockTest.java
@@ -0,0 +1,11 @@
+package day50_inheritance;
+
+public class StaticBlockTest {
+    public static void main(String[] args) {
+        StaticBlockDemo stb1 = new StaticBlockDemo(); // static block will run, 1 time only before everything else
+        StaticBlockDemo stb2 = new StaticBlockDemo(); // for the second time static block will not run
+        StaticBlockDemo stb3 = new StaticBlockDemo();
+        System.out.println(StaticBlockDemo.num);
+        System.out.println(StaticBlockDemo.carModels);
+    }
+}
