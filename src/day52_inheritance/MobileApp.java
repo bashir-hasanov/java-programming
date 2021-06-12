0ef3bcfb7e7270bae2d7eff4404535092238c557
Bashir Hasanov <beshir.hasanov@gmail.com>
2021-06-12 11:45:05 -0400

    created new package and 3 classes - MobileApp example

---
+++ src/day52_inheritance/MobileApp.java
@@ -0,0 +1,10 @@
+package day52_inheritance;
+
+public class MobileApp {
+    int name;
+    double version;
+
+    public void useTheApp(int minutes) {
+        System.out.println("Using " + name + " app for " + minutes + " minutes");
+    }
+}
