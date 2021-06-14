1e8b146b4224aa672d15b038d7ff5eb3b748ed38
Bashir Hasanov <beshir.hasanov@gmail.com>
2021-06-14 18:26:58 -0400

    Added 2 child classes - Take5 and Twix

---
+++ src/Office_Hours/practice_06_14_2021/Take5.java
@@ -0,0 +1,10 @@
+package Office_Hours.practice_06_14_2021;
+
+public class Take5 extends Candy {
+
+    public Take5(int quantity, boolean hasPeanuts) {
+        super("Reese", quantity, hasPeanuts);
+    }
+
+
+}
---
+++ src/Office_Hours/practice_06_14_2021/Twix.java
@@ -0,0 +1,8 @@
+package Office_Hours.practice_06_14_2021;
+
+public class Twix extends Candy {
+
+    public Twix(int quantity, boolean hasPeanuts) {
+        super("Mars", quantity, hasPeanuts);
+    }
+}
